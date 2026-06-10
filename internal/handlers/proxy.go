ad614de09bb5f9c33bc6892a9b26dfcb2941faf0
arjun118 <vishnutejach3@gmail.com>
2026-06-11 01:41:51 +0530

    phase 2: added some headers, yet to limit cache-control

--- internal/handlers/proxy.go
+++ internal/handlers/proxy.go
@@ -4,10 +4,13 @@ import (
 	"io"
 	"net/http"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/minio/minio-go/v7"
 )
+
+//cache control to be implemented
 
 type ProxyHandler struct {
 	MinioClient *minio.Client
@@ -15,6 +18,15 @@ type ProxyHandler struct {
 }
 
 func (p *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet &&
+		r.Method != http.MethodHead {
+		http.Error(
+			w,
+			"method not allowed",
+			http.StatusMethodNotAllowed,
+		)
+		return
+	}
 	objectKey := strings.TrimPrefix(r.URL.Path, "/media/")
 	if objectKey == "" {
 		http.Error(w, "missing object key", http.StatusBadRequest)
@@ -27,16 +39,34 @@ func (p *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer obj.Close()
-	var contentType string
-	switch filepath.Ext(objectKey) {
-	case ".m3u8":
-		contentType = "application/vnd.apple.mpegurl"
-	case ".ts":
-		contentType = "video/mp2t"
-	default:
-		contentType = "application/octet-stream"
+	info, err := obj.Stat()
+	if err != nil {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
+	contentType := info.ContentType
+	if contentType == "" {
+		switch filepath.Ext(objectKey) {
+		case ".m3u8":
+			contentType = "application/vnd.apple.mpegurl"
+		case ".ts":
+			contentType = "video/mp2t"
+		default:
+			contentType = "application/octet-stream"
+		}
+	}
+	//segment not modified
+	if r.Header.Get("If-None-Match") == info.ETag {
+		w.WriteHeader(http.StatusNotModified)
+		return
 	}
 	w.Header().Set("Content-Type", contentType)
-
+	w.Header().Set("Content-Length", strconv.FormatInt(info.Size, 10))
+	w.Header().Set("ETag", info.ETag)
+	w.Header().Set("Last-Modified", info.LastModified.UTC().Format(http.TimeFormat))
+	if r.Method == http.MethodHead {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 	_, _ = io.Copy(w, obj)
 }
